606c78fceafd012658eb237c7bf4f07a81c82248
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-09-12 18:33:34 -0300

    Problem complete ✅

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,22 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+  // Binary search only works on a sorted array
 
+    let min = 0;
+    let max = haystack.length;
+    
+    do {
+      let midPoint = Math.floor(min + (max - min) / 2);
+      const value = haystack[midPoint];
+
+      if (value === needle) {
+        return true;
+      } else if (value < needle) {
+        min = midPoint + 1;
+      } else {
+        max = midPoint;
+      }
+
+    } while (min < max);
+
+    return false;
 }
