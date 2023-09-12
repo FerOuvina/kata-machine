a5754e5ca4c5d54bba013caa296b2c0b041e90f0
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-09-12 19:40:38 -0300

    Problem complete ✅

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,11 @@
 export default function bubble_sort(arr: number[]): void {
+    for (let i = 0; i < arr.length; i++) {
+        const x = arr[i];
 
+        if (x > arr[i + 1]) {
+            arr[i] = arr[i + 1];
+            arr[i + 1] = x;
+            i = -1;
+        }
+    }
 }
